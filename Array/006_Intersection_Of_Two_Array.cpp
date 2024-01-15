eaac09fc3c815690d608028b170f4e566183a481
Avdhesh Bapodara <avdhesh.bict21@sot.pdpu.ac.in>
2024-01-15 19:24:33 +0530

    15 Jan 24

--- Array/006_Intersection_Of_Two_Array.cpp
+++ Array/006_Intersection_Of_Two_Array.cpp
@@ -4,32 +4,65 @@ using namespace std;
 // Today's Date - 14/01/24
 // Leetcode Question Name - Intersection of Two Arrays
 
+// class Solution
+// {
+// public:
+//     vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
+//     {
+//         vector<int> temp;
+//         unordered_set<int> s1;
+//         unordered_set<int> s2;
+//         unordered_set<int> s3;
+//         for (auto &ele1 : nums1)
+//             s1.insert(ele1);
+//         for (auto &ele2 : nums2)
+//             s2.insert(ele2);
+
+//         for (int i = 0; i < nums1.size(); i++)
+//         {
+//             // cout << nums1[i] << " Jai Shree Sita" << endl;
+//             if (s2.find(nums1[i]) != s2.end())
+//                 s3.insert(nums1[i]);
+//         }
+//         for (auto it = s3.begin(); it != s3.end(); ++it)
+//             temp.push_back(*it);
+//         return temp;
+//     }
+// };
 class Solution
 {
 public:
     vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
     {
-        for (int i = 0; i < nums2.size(); i++)
-            nums1.push_back(nums2[i]);
+        vector<int> check(1001, 0);
 
-        unordered_map<int, int> m;
-        for (auto &ele : nums1)
-            m[ele]++;
+        for (int i = 0; i < nums1.size(); ++i)
+            check[nums1[i]] = 1;
 
+        for (int i = 0; i < nums2.size(); ++i)
+        {
+            if (check[nums2[i]] == 1)
+                check[nums2[i]] = 2;
+        }
+
+        nums1.clear();
+
+        for (int i = 0; i < check.size(); i++)
+            if (check[i] == 2)
+                nums1.push_back(i);
         return nums1;
     }
 };
 
 int main()
 {
-    vector<int> vec1 = {9, 4, 9, 8, 4};
-    vector<int> vec2 = {4, 9, 5};
+    vector<int> vec1 = {4, 7, 9, 7, 6, 7};
+    vector<int> vec2 = {5, 0, 0, 6, 1, 6, 2, 2, 4};
     Solution solution;
     vector<int> vector_final = solution.intersection(vec1, vec2);
-    for (int i = 0; i < vec1.size() + vec2.size(); i++)
+    for (int i = 0; i < vector_final.size(); i++)
     {
         cout << vector_final[i] << endl;
     }
-
     return 0;
 }
